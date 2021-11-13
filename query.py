104cfcbea9c2419dc7278a125a9a62b6456fec49
Salvatore Logozzo <salvatorelogozzo@Salvatores-MacBook-Pro.local>
2021-11-13 15:21:24 -0500

    Added Query print statement and Connection closing statements

--- query.py
+++ query.py
@@ -2,11 +2,17 @@ import connect
 import cx_Oracle
 
 cursor = connect.connection.cursor()
-sql_drop = """
+query = """
 SELECT ROLE, COUNT(USER_ID) AS COUNT
     FROM usertb
     GROUP BY ROLE
 """
 
-cursor.execute(sql_drop)
+cursor.execute(query)
+row = cursor.fetchall()
+print(row)
 print('Query Executed.')
+
+#Closing cursor and connection
+cursor.close()
+connect.connection.close()
