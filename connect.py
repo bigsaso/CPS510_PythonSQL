07e0a64ee84f63a21a723a2a07343b1804841078
Salvatore Logozzo <salvatorelogozzo@Salvatores-MacBook-Pro.local>
2021-11-13 14:02:35 -0500

    Modified connection

--- connect.py
+++ connect.py
@@ -1,6 +1,6 @@
 import cx_Oracle
 
 #Creating connection with DBMS
-connection = cx_Oracle.connect('slogozzo/11236504@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca')
+connection = cx_Oracle.connect('slogozzo/11236504@//oracle.scs.ryerson.ca:1521/orcl')
 #Checking connection status
 print(connection.version)
